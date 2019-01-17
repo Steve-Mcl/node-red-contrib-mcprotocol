aa05e6f0dce5e56b69ff4a441555793cb414fe93
Steve-Mcl <steve.mclaughlin@gmail.com>
2019-01-17 18:02:26 +0000

    refactoring & better handling of auto(re)connect

--- connection_pool.js
+++ connection_pool.js
@@ -1,6 +1,7 @@
 var util = require("util");
 
-var clients = {};
+
+var pool = {};
 
 
 // function convertPayloadToDataArray(payload) {
@@ -27,108 +28,130 @@ var clients = {};
 
 
 module.exports = {
-  
+
   get: function (port,host,opts) {
     var mcprotocol = require('./mcprotocol/mcprotocol.js');
     var id = `mcprotocol: {host:'${(host || "")}', port: ${(port || "''")}}`;
     //var id = `mcprotocol: {host:'${(host || "")}', port: ${(port || "''")}, frame:'${frame}', plcType:'${plcType}}`;
     var node = this;
     
-    if (!clients[id]) {
-      clients[id] = function () {
+    if (!pool[id]) {
+      pool[id] = function () {
         var options = opts || {};
         options.host = host;
         options.port = port;
         options.ascii = options.ascii || false;
         options.frame = options.frame || "3E";
         options.plcType = options.plcType || "Q";
-
+        options.autoConnect = options.autoConnect == "undefined" ? true : options.autoConnect;
+        options.preventAutoReconnect = false;
+        
         util.log(`[mcprotocol] adding new connection to pool ~ ${id}`);
 
-        var client = new mcprotocol();
-        client.setDebugLevel("WARN");//
-        client.initiateConnection(options);
+        var mcp = new mcprotocol();
+        
+        mcp.setDebugLevel("WARN");//
+        mcp.initiateConnection(options);
         var connecting = false;
         var obj = {
-
+          getMCP: function mcp() {
+            return mcp;
+          },
+          getAutoConnect: function() {
+            return (options.autoConnect == true);
+          },
+          setAutoConnect: function(b) {
+            options.autoConnect = (b == true);
+          },
           _instances: 0,
           write: function (addr, data, callback) {
-            if(!client.isConnected()){
+            if(!mcp.isConnected()){
               this.connect();
               throw new Error("Not connected!")
             }
-            var reply = client.writeItems(addr, data, callback);
+            var reply = mcp.writeItems(addr, data, callback);
             return reply;
           },
           read: function (addr, callback) {
-            if(!client.isConnected()){
+            if(!mcp.isConnected()){
               this.connect();
               throw new Error("Not connected!")
             }
-            var reply = client.readItems(addr, callback);
+            var reply = mcp.readItems(addr, callback);
             return reply;
           },
           closeConnection: function(){
-            client.connectionReset();
+            mcp.connectionReset();
+            options.preventAutoReconnect = true;
           },
           on: function (a, b) {
-            client.on(a, b);
+            mcp.on(a, b);
           },
           connect: function () {
-            if (client && !client.isConnected() && !connecting) {
+            options.preventAutoReconnect = false;
+            if (mcp && !mcp.isConnected() && !connecting) {
               connecting = true;
-              client.initiateConnection(options);
+              mcp.initiateConnection(options);
             }
           },
           
           // decodeMemoryAddress : function (addressString)  {
-          //   return client.decodeMemoryAddress(addressString);
+          //   return mcp.decodeMemoryAddress(addressString);
           // },
 
           // decodedAddressToString : function (decodedAddress, offsetWD, offsetBit)  {
-          //   return client.decodedAddressToString(decodedAddress, offsetWD, offsetBit);
+          //   return mcp.decodedAddressToString(decodedAddress, offsetWD, offsetBit);
           // },
 
           disconnect: function () {
             this._instances -= 1;
             if (this._instances <= 0) {
               util.log(`[mcprotocol] closing connection ~ ${id}`);
-              client.dropConnection();
-              client = null;
+              mcp.dropConnection();
+              mcp = null;
               util.log(`[mcprotocol] deleting connection from pool ~ ${id}`);
-              delete clients[id];
+              delete pool[id];
             }
           }
         };
         
-        client.on('error', function (e) {
-          if (client) {
+        mcp.on('error', function (e) {
+          if (mcp) {
             util.log(`[mcprotocol] error ~ ${id}: ${e}`);
             connecting = false;
-            setTimeout(function () {
-              obj.connect();
-            },  1000); //parameterise
+            if(options.autoConnect){
+              setTimeout(function () {
+                if(options.autoConnect && !options.preventAutoReconnect){
+                  util.log(`[mcprotocol] autoConnect call from  error handler ~ ${id}`);
+                  obj.connect();
+                }
+              },  1000); //parameterise
+            }
           }
         });
-        client.on('open', function () {
-          if (client) {
+        mcp.on('open', function () {
+          if (mcp) {
             util.log(`[mcprotocol] connected ~ ${id}`);
             connecting = false;
-            
           }
         });
-        client.on('close', function (err) {
+        mcp.on('close', function (err) {
           util.log(`[mcprotocol] connection closed ~ ${id}`);
           connecting = false;
-          setTimeout(function () {
-            obj.connect();
-          },  1000); //parameterise
+          if(options.autoConnect){
+            setTimeout(function () {
+              if(options.autoConnect && !options.preventAutoReconnect){
+                util.log(`[mcprotocol] autoConnect call from close handler ~ ${id}`);
+                obj.connect();
+              }
+            },  1000); //parameterise
+          }
         });
 
         return obj
       }();
     }
-    clients[id]._instances += 1;
-    return clients[id];
+    pool[id]._instances += 1;
+    return pool[id];
   }
 };
