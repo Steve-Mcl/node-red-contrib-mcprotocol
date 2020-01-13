551db107d91b362201080b57529e987e5b5c4c18
Steve-Mcl <steve.mclaughlin@gmail.com>
2020-01-13 09:40:07 +0000

    code formatting

--- nodes/read.js
+++ nodes/read.js
@@ -1,218 +1,235 @@
-module.exports = function (RED) {
-	var connection_pool = require("../connection_pool.js");
-	
-	function mcRead(config) {
-		RED.nodes.createNode(this, config);
+module.exports = function(RED) {
+  var connection_pool = require("../connection_pool.js");
+
+  function mcRead(config) {
+    RED.nodes.createNode(this, config);
     this.name = config.name;
     this.topic = config.topic;
     this.connection = config.connection;
-		this.address = config.address || "";//address
-		this.addressType = config.addressType || "str";
-		this.outputFormat = config.outputFormat || 0;
+    this.address = config.address || ""; //address
+    this.addressType = config.addressType || "str";
+    this.outputFormat = config.outputFormat || 0;
 
     this.connectionConfig = RED.nodes.getNode(this.connection);
     var context = this.context();
     var node = this;
-		node.busy = false;
-		//node.busyMonitor;
-		node.busyTimeMax = 1000;//TODO: Parameterise hard coded value!
+    node.busy = false;
+    //node.busyMonitor;
+    node.busyTimeMax = 1000; //TODO: Parameterise hard coded value!
     //var mcprotocol = require('../mcprotocol.js');
     if (this.connectionConfig) {
-			var options = Object.assign({}, node.connectionConfig.options);
-      node.connection = connection_pool.get(this.connectionConfig.port, this.connectionConfig.host, options);
-      node.status({fill:"yellow",shape:"ring",text:"initialising"});
+      var options = Object.assign({}, node.connectionConfig.options);
+      node.connection = connection_pool.get(
+        this.connectionConfig.port,
+        this.connectionConfig.host,
+        options
+      );
+      node.status({ fill: "yellow", shape: "ring", text: "initialising" });
 
-      this.connection.on('error', function (error) {
-        console.log("Error: ", error);
-				node.status({fill:"red",shape:"ring",text:"error"});
-				node.busy = false;
+      this.connection.on("error", function(error) {
+        console.error(error);
+        node.status({ fill: "red", shape: "ring", text: "error" });
+        node.busy = false;
       });
-      this.connection.on('open', function (error) {
-        node.status({fill:"green",shape:"dot",text:"connected"});
+      this.connection.on("open", function(error) {
+        node.status({ fill: "green", shape: "dot", text: "connected" });
       });
-      this.connection.on('close', function (error) {
-				node.status({fill:"red",shape:"dot",text:"not connected"});
-				node.busy = false;
+      this.connection.on("close", function(error) {
+        node.status({ fill: "red", shape: "dot", text: "not connected" });
+        node.busy = false;
       });
 
-
-			function myReply(problem, msg) {
-				clearTimeout(node.busyMonitor);
-				if(!node.busy){
-          return;//not busy - dont process the reply (node may have timed out)
+      function myReply(problem, msg) {
+        clearTimeout(node.busyMonitor);
+        if (!node.busy) {
+          return; //not busy - dont process the reply (node may have timed out)
         }
-				node.busy = false;//reset busy - allow node to be triggered
-				node.msgMem.mcReadDetails = {};
-				node.msgMem.mcReadDetails.request = node.request;
-				node.msgMem.mcReadDetails.response = msg;
-				node.msgMem.mcReadDetails.timeout = msg.timeout;//TODO
-				node.msgMem.mcReadDetails.error = problem;
-				node.msgMem.payload = null;
+        node.busy = false; //reset busy - allow node to be triggered
+        node.msgMem.mcReadDetails = {};
+        node.msgMem.mcReadDetails.request = node.request;
+        node.msgMem.mcReadDetails.response = msg;
+        node.msgMem.mcReadDetails.timeout = msg.timeout; //TODO
+        node.msgMem.mcReadDetails.error = problem;
+        node.msgMem.payload = null;
 
-        if(msg.timeout)  {
-          node.status({fill:"red",shape:"ring",text:"timeout"});
-					node.error("timeout");
-					var dbgmsg = {
-            f: 'myReply(msg)',
+        if (msg.timeout) {
+          node.status({ fill: "red", shape: "ring", text: "timeout" });
+          node.error("timeout");
+          var dbgmsg = {
+            f: "myReply(msg)",
             msg: msg,
-            error: 'timeout'
-          }
-					console.error(dbgmsg);
-					node.msgMem.mcReadDetails.errorMsg = 'timeout';
-					node.send(node.msgMem);
+            error: "timeout"
+          };
+          console.error(dbgmsg);
+          node.msgMem.mcReadDetails.errorMsg = "timeout";
+          node.send(node.msgMem);
           return;
-				}
+        }
 
-				if(problem)  {
-          node.status({fill:"grey",shape:"ring",text:"Quality Issue"});
-				} else {
-					node.status({fill:"green",shape:"dot",text:"Good"});
-				}
+        if (problem) {
+          node.status({ fill: "grey", shape: "ring", text: "Quality Issue" });
+        } else {
+          node.status({ fill: "green", shape: "dot", text: "Good" });
+        }
 
-				// msg.deviceCode
-				// msg.digitSpec
-				// msg.dataType
-				// msg.deviceNo 
-				// msg.isGood 
-				// msg.quality 
-				// msg.TAG 
-				// msg.addr 
-				// msg.timeTaken 
-				// msg.timeStamp 
-				// msg.value 
-				// msg.valueType 
+        // msg.deviceCode
+        // msg.digitSpec
+        // msg.dataType
+        // msg.deviceNo
+        // msg.isGood
+        // msg.quality
+        // msg.TAG
+        // msg.addr
+        // msg.timeTaken
+        // msg.timeStamp
+        // msg.value
+        // msg.valueType
 
-				var data = msg.value;
-				if(data && !problem) {
-					let iWD = msg.deviceNo;
-					let loopBit = 0, bitNo = msg.bitOffset;
-					let JSONData = {};
-					if(node.outputFormat == 0/*JSON*/){
-						if(msg.valueType == "CHAR") {
-							if(msg.deviceCodeNotation == 'Hexadecimal'){
-								buff_address = `${msg.deviceCode}${Number(iWD).toString(16).toUpperCase()}`;
-							} else {
-								buff_address = `${msg.deviceCode}${iWD}`
-							}
-							JSONData[buff_address] =  data;
-						} else {
-							for (var x in data) {
-								let buff_address = '';
-								if(msg.dataType == 'BIT' && msg.deviceCodeType != "BIT"){
-									bitNo = msg.bitOffset + loopBit;
-									if(bitNo == 16) iWD++;
-									if(bitNo >= 16){
-										bitNo = bitNo - 16
-									}
-									if(msg.deviceCodeNotation == 'Hexadecimal'){
-										buff_address = `${msg.deviceCode}${Number(iWD).toString(16).toUpperCase()}.${Number(bitNo).toString(16).toUpperCase()}`
-									} else {
-										buff_address = `${msg.deviceCode}${iWD}.${Number(bitNo).toString(16).toUpperCase()}`
-									}
-									JSONData[buff_address] =  data[x];
-									loopBit++;
-									if(loopBit >= 16)
-										loopBit = 0;
-								} else {
-									if(msg.deviceCodeNotation == 'Hexadecimal'){
-										buff_address = `${msg.deviceCode}${Number(iWD).toString(16).toUpperCase()}`
-									} else {
-										buff_address = `${msg.deviceCode}${iWD}`
-									}
-									JSONData[buff_address] =  data[x];
-									iWD += (msg.dataTypeByteLength/2);
-								}
-								
-							}
-						}
-						node.msgMem.payload = JSONData;
-					} else {
-						node.msgMem.payload = data;
-					}
-				}
+        var data = msg.value;
+        if (data && !problem) {
+          let iWD = msg.deviceNo;
+          let loopBit = 0,
+            bitNo = msg.bitOffset;
+          let JSONData = {};
+          if (node.outputFormat == 0 /*JSON*/) {
+            if (msg.valueType == "CHAR") {
+              if (msg.deviceCodeNotation == "Hexadecimal") {
+                buff_address = `${msg.deviceCode}${Number(iWD)
+                  .toString(16)
+                  .toUpperCase()}`;
+              } else {
+                buff_address = `${msg.deviceCode}${iWD}`;
+              }
+              JSONData[buff_address] = data;
+            } else {
+              for (var x in data) {
+                let buff_address = "";
+                if (msg.dataType == "BIT" && msg.deviceCodeType != "BIT") {
+                  bitNo = msg.bitOffset + loopBit;
+                  if (bitNo == 16) iWD++;
+                  if (bitNo >= 16) {
+                    bitNo = bitNo - 16;
+                  }
+                  if (msg.deviceCodeNotation == "Hexadecimal") {
+                    buff_address = `${msg.deviceCode}${Number(iWD)
+                      .toString(16)
+                      .toUpperCase()}.${Number(bitNo)
+                      .toString(16)
+                      .toUpperCase()}`;
+                  } else {
+                    buff_address = `${msg.deviceCode}${iWD}.${Number(bitNo)
+                      .toString(16)
+                      .toUpperCase()}`;
+                  }
+                  JSONData[buff_address] = data[x];
+                  loopBit++;
+                  if (loopBit >= 16) loopBit = 0;
+                } else {
+                  if (msg.deviceCodeNotation == "Hexadecimal") {
+                    buff_address = `${msg.deviceCode}${Number(iWD)
+                      .toString(16)
+                      .toUpperCase()}`;
+                  } else {
+                    buff_address = `${msg.deviceCode}${iWD}`;
+                  }
+                  JSONData[buff_address] = data[x];
+                  iWD += msg.dataTypeByteLength / 2;
+                }
+              }
+            }
+            node.msgMem.payload = JSONData;
+          } else {
+            node.msgMem.payload = data;
+          }
+        }
         node.send(node.msgMem);
       }
 
-			this.on('input', function (msg) {
-				if(msg.disconnect === true || msg.topic === 'disconnect'){
-					this.connection.closeConnection();
-					return;
-				} else if(msg.connect === true || msg.topic === 'connect'){
-					this.connection.connect();
-					return;
-        } 
-         
-				if(node.busy)
-					return;//TODO: Consider queueing inputs?
+      this.on("input", function(msg) {
+        if (msg.disconnect === true || msg.topic === "disconnect") {
+          this.connection.closeConnection();
+          return;
+        } else if (msg.connect === true || msg.topic === "connect") {
+          this.connection.connect();
+          return;
+        }
 
-				node.request = undefined;
-				node.msgMem = msg;
+        if (node.busy) return; //TODO: Consider queueing inputs?
 
-				var addr;
-				RED.util.evaluateNodeProperty(node.address,node.addressType,node,msg,(err,value) => {
-					if (err) {
-						node.error("Unable to evaluate address");
-						node.status({fill:"red",shape:"ring",text:"Unable to evaluate address"});
-						return;
-					} else {
-						addr = value;
-					}
-				}); 
+        node.request = undefined;
+        node.msgMem = msg;
 
-				if(addr == "")	{
-					node.error("address is empty");
-					node.status({fill:"red",shape:"ring",text:"error"});
-					return;
-				}
+        var addr;
+        RED.util.evaluateNodeProperty(
+          node.address,
+          node.addressType,
+          node,
+          msg,
+          (err, value) => {
+            if (err) {
+              node.error("Unable to evaluate address");
+              node.status({
+                fill: "red",
+                shape: "ring",
+                text: "Unable to evaluate address"
+              });
+              return;
+            } else {
+              addr = value;
+            }
+          }
+        );
 
+        if (addr == "") {
+          node.error("address is empty");
+          node.status({ fill: "red", shape: "ring", text: "error" });
+          return;
+        }
 
-				try {
-					node.status({fill:"yellow",shape:"ring",text:"read"});
-					node.busy = true;
+        try {
+          node.status({ fill: "yellow", shape: "ring", text: "read" });
+          node.busy = true;
 
-					node.request = { 
-						outputFormat: node.outputFormat ? 'Array' : 'JSON',
-						address: addr,
+          node.request = {
+            outputFormat: node.outputFormat ? "Array" : "JSON",
+            address: addr,
             timeStamp: Date.now()
-					};
+          };
 
-					if (node.busyTimeMax) {
-						node.busyMonitor = setTimeout(function() {
-							if(node.busy){
-								node.status({fill:"red",shape:"ring",text:"timeout"});
-								node.error("timeout");
-								node.busy = false;
-								return;
-							}
-						}, node.busyTimeMax);
-					}
-					this.connection.read(addr, myReply);
-				} catch (error) {
+          if (node.busyTimeMax) {
+            node.busyMonitor = setTimeout(function() {
+              if (node.busy) {
+                node.status({ fill: "red", shape: "ring", text: "timeout" });
+                node.error("timeout");
+                node.busy = false;
+                return;
+              }
+            }, node.busyTimeMax);
+          }
+          this.connection.read(addr, myReply);
+        } catch (error) {
           node.busy = false;
           node.error(error);
-					node.status({fill:"red",shape:"ring",text:"error"});
-					var dbgmsg = { 
-						info: "read.js-->on 'input'",
-            connection: `host: ${node.connectionConfig.host}, port: ${node.connectionConfig.port}`, 
-            address: addr,
-					 };
-					console.debug(dbgmsg);
+          node.status({ fill: "red", shape: "ring", text: "error" });
+          var dbgmsg = {
+            info: "read.js-->on 'input'",
+            connection: `host: ${node.connectionConfig.host}, port: ${node.connectionConfig.port}`,
+            address: addr
+          };
+          console.debug(dbgmsg);
           return;
-				}
-				
-			});
-			node.status({fill:"green",shape:"ring",text:"ready"});
-
-		} else {
-			node.error("configuration not setup");
-			node.status({fill:"red",shape:"ring",text:"error"});
+        }
+      });
+      node.status({ fill: "green", shape: "ring", text: "ready" });
+    } else {
+      node.error("configuration not setup");
+      node.status({ fill: "red", shape: "ring", text: "error" });
     }
-	}
-	RED.nodes.registerType("MC Read", mcRead);
-	mcRead.prototype.close = function() {
-		if (this.connection) {
-			this.connection.disconnect();
-		}
-	}
+  }
+  RED.nodes.registerType("MC Read", mcRead);
+  mcRead.prototype.close = function() {
+    if (this.connection) {
+      this.connection.disconnect();
+    }
+  };
 };
-
--- nodes/write.js
+++ nodes/write.js
@@ -1,4 +1,4 @@
-module.exports = function (RED) {
+module.exports = function(RED) {
   var connection_pool = require("../connection_pool.js");
   var util = require("util");
 
@@ -10,139 +10,165 @@ module.exports = function (RED) {
     this.connection = config.connection;
     this.address = config.address;
     this.data = config.data;
-    this.address = config.address || "";//address
+    this.address = config.address || ""; //address
     this.addressType = config.addressType || "str";
-    this.data = config.data || "";//data
+    this.data = config.data || ""; //data
     this.dataType = config.dataType || "num";
 
     this.connectionConfig = RED.nodes.getNode(this.connection);
     var context = this.context();
     var node = this;
     node.busy = false;
-		node.busyTimeMax = 1000;//TODO: Parameterise hard coded value!
+    node.busyTimeMax = 1000; //TODO: Parameterise hard coded value!
     //var mcprotocol = require('../mcprotocol.js');
     if (this.connectionConfig) {
+      node.status({ fill: "yellow", shape: "ring", text: "initialising" });
+      var options = Object.assign({}, node.connectionConfig.options);
+      this.connection = connection_pool.get(
+        this.connectionConfig.port,
+        this.connectionConfig.host,
+        options
+      );
 
-      node.status({fill:"yellow",shape:"ring",text:"initialising"});
-			var options = Object.assign({}, node.connectionConfig.options);
-      this.connection = connection_pool.get(this.connectionConfig.port, this.connectionConfig.host, options);
-
-      this.connection.on('error', function (error) {
-        console.log("Error: ", error);
-        node.status({fill:"red",shape:"ring",text:"error"});
+      this.connection.on("error", function(error) {
+        console.error(error);
+        node.status({ fill: "red", shape: "ring", text: "error" });
       });
-      this.connection.on('open', function (error) {
-        node.status({fill:"green",shape:"dot",text:"connected"});
+      this.connection.on("open", function(error) {
+        node.status({ fill: "green", shape: "dot", text: "connected" });
       });
-      this.connection.on('close', function (error) {
-        node.status({fill:"red",shape:"dot",text:"not connected"});
+      this.connection.on("close", function(error) {
+        node.status({ fill: "red", shape: "dot", text: "not connected" });
       });
 
       function myReply(problem, msg) {
-        node.busy = false;//reset busy - allow node to be triggered
+        node.busy = false; //reset busy - allow node to be triggered
         clearTimeout(node.busyMonitor);
 
-        if(msg.timeout)  {
-          node.status({fill:"red",shape:"ring",text:"timeout"});
+        if (msg.timeout) {
+          node.status({ fill: "red", shape: "ring", text: "timeout" });
           node.error("timeout");
           var dbgmsg = {
-            f: 'myReply(msg)',
+            f: "myReply(msg)",
             msg: msg,
-            error: 'timeout'
-          }
+            error: "timeout"
+          };
           console.error(dbgmsg);
-          return;//halt flow
+          return; //halt flow
         }
 
-				if(problem)  {
-          node.status({fill:"grey",shape:"ring",text:"Quality Issue"});
-				} else {
-					node.status({fill:"green",shape:"dot",text:"Good"});
-				}
-        
+        if (problem) {
+          node.status({ fill: "grey", shape: "ring", text: "Quality Issue" });
+        } else {
+          node.status({ fill: "green", shape: "dot", text: "Good" });
+        }
+
         node.msgMem.payload = !problem;
         node.msgMem.mcWriteDetails = msg;
         node.send(node.msgMem);
-        
       }
-      this.on('input', function (msg) {
-				if(msg.disconnect === true || msg.topic === 'disconnect'){
-					this.connection.closeConnection();
-					return;
-				} else if(msg.connect === true || msg.topic === 'connect'){
-					this.connection.connect();
-					return;
-        } 
-         
-        if (node.busy)
-          return;//TODO: Consider queueing inputs?
+      this.on("input", function(msg) {
+        if (msg.disconnect === true || msg.topic === "disconnect") {
+          this.connection.closeConnection();
+          return;
+        } else if (msg.connect === true || msg.topic === "connect") {
+          this.connection.connect();
+          return;
+        }
+
+        if (node.busy) return; //TODO: Consider queueing inputs?
         node.request = undefined;
         node.msgMem = msg;
 
         var isObject = function(val) {
-          if (val === null) { return false;}
-          return ( (typeof val === 'function') || (typeof val === 'object') );
-        }
-        var addr;// = /* node.address || */ config.address || msg.payload.address; 
+          if (val === null) {
+            return false;
+          }
+          return typeof val === "function" || typeof val === "object";
+        };
+        var addr; // = /* node.address || */ config.address || msg.payload.address;
         var data;
         //address - address
-        RED.util.evaluateNodeProperty(node.address, node.addressType, node, msg, (err, value) => {
-          if (err) {
-            node.error("Unable to evaluate address");
-            node.status({ fill: "red", shape: "ring", text: "Unable to evaluate address" });
-            return;
-          } else {
-            addr = value;
-            if (addr == "") {
-              node.error("address is empty");
-              node.status({ fill: "red", shape: "ring", text: "address is empty" });
+        RED.util.evaluateNodeProperty(
+          node.address,
+          node.addressType,
+          node,
+          msg,
+          (err, value) => {
+            if (err) {
+              node.error("Unable to evaluate address");
+              node.status({
+                fill: "red",
+                shape: "ring",
+                text: "Unable to evaluate address"
+              });
               return;
+            } else {
+              addr = value;
+              if (addr == "") {
+                node.error("address is empty");
+                node.status({
+                  fill: "red",
+                  shape: "ring",
+                  text: "address is empty"
+                });
+                return;
+              }
             }
           }
-        });
-
+        );
 
         //data - data
-        console.log("node.data...")       ;
-        console.log(node.data)       ;
+        console.log("node.data...");
+        console.log(node.data);
 
         // var csv2arr = function (str) {
         //   return node.data.split(',').map(Number);
         // }
-        if (node.dataType == 'str') {
-          console.log("mcwrite input:node.dataType == 'str'")       ;
+        if (node.dataType == "str") {
+          console.log("mcwrite input:node.dataType == 'str'");
           data = node.data;
-        } else if (node.dataType == 'num') {
-          console.log("mcwrite input:node.dataType == 'num'")       ;
+        } else if (node.dataType == "num") {
+          console.log("mcwrite input:node.dataType == 'num'");
           //data = [node.data];
           data = node.data;
-        } else if (node.dataType == 'csv') {
-          console.log("mcwrite input:node.dataType == 'csv'")       ;
+        } else if (node.dataType == "csv") {
+          console.log("mcwrite input:node.dataType == 'csv'");
           //data = csv2arr(node.data);
           data = node.data;
         } else {
-          console.log("mcwrite input:evaluateNodeProperty")       ;
-          RED.util.evaluateNodeProperty(node.data, node.dataType, node, msg, (err, value) => {
-            if (err) {
-              node.error("Unable to evaluate data");
-              node.status({ fill: "red", shape: "ring", text: "Unable to evaluate data" });
-              return;
-            } else {
-              // if (typeof value === "string") {
-              //   data = csv2arr(value);
-              // } else {
-              //   data = value;
-              // }
-              data = value;
+          console.log("mcwrite input:evaluateNodeProperty");
+          RED.util.evaluateNodeProperty(
+            node.data,
+            node.dataType,
+            node,
+            msg,
+            (err, value) => {
+              if (err) {
+                node.error("Unable to evaluate data");
+                node.status({
+                  fill: "red",
+                  shape: "ring",
+                  text: "Unable to evaluate data"
+                });
+                return;
+              } else {
+                // if (typeof value === "string") {
+                //   data = csv2arr(value);
+                // } else {
+                //   data = value;
+                // }
+                data = value;
+              }
             }
-          });
+          );
         }
 
-        if(!data)	{
-					node.error("Data is empty");
-					return;
+        if (!data) {
+          node.error("Data is empty");
+          return;
         }
-        
+
         // if (typeof data == "number") {
         //   console.log('mcwrite input:data == "number"')       ;
         //   data = [data];
@@ -153,55 +179,52 @@ module.exports = function (RED) {
         //   node.status({ fill: "red", shape: "ring", text: "data length and count are not equal" });
         //   return;
         // }
-        
+
         try {
-          node.status({fill:"yellow",shape:"ring",text:"write"});
+          node.status({ fill: "yellow", shape: "ring", text: "write" });
           node.busy = true;
           if (node.busyTimeMax) {
             this.busyMonitor = setTimeout(function() {
-              if(node.busy){
-                node.status({fill:"red",shape:"ring",text:"timeout"});
+              if (node.busy) {
+                node.status({ fill: "red", shape: "ring", text: "timeout" });
                 node.error("timeout");
                 node.busy = false;
                 return;
               }
             }, node.busyTimeMax);
-          }   
+          }
 
-          console.log("mcwrite input:  addr,  data...")       ;
-          console.log(addr)       ;
-          console.log(data)       ;
+          console.log("mcwrite input:  addr,  data...");
+          console.log(addr);
+          console.log(data);
 
           this.connection.write(addr, data, myReply);
         } catch (error) {
           node.busy = false;
           node.error(error);
-          node.status({fill:"red",shape:"ring",text:"error"});
-          var dbgmsg = { 
-						info: "write.js-->on 'input' - try this.connection.write(addr, data, myReply)",
-            connection: `host: ${node.connectionConfig.host}, port: ${node.connectionConfig.port}`, 
+          node.status({ fill: "red", shape: "ring", text: "error" });
+          var dbgmsg = {
+            info:
+              "write.js-->on 'input' - try this.connection.write(addr, data, myReply)",
+            connection: `host: ${node.connectionConfig.host}, port: ${node.connectionConfig.port}`,
             address: addr,
             data: data,
-						error: error
-					 };
-					console.debug(dbgmsg);
+            error: error
+          };
+          console.debug(dbgmsg);
           return;
         }
-        
       });
-      node.status({fill:"green",shape:"ring",text:"ready"});
-
+      node.status({ fill: "green", shape: "ring", text: "ready" });
     } else {
       node.error("configuration not setup");
-      node.status({fill:"red",shape:"ring",text:"error"});
+      node.status({ fill: "red", shape: "ring", text: "error" });
     }
-
   }
   RED.nodes.registerType("MC Write", mcWrite);
   mcWrite.prototype.close = function() {
-		if (this.connection) {
-			this.connection.disconnect();
-		}
-	}
+    if (this.connection) {
+      this.connection.disconnect();
+    }
+  };
 };
-
